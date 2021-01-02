1bfe4fd1f91605ee9adcaedf1df245b5958ae17f
Shpat Jakupi <shpatjakupi@Shpats-MacBook-Pro.local>
2021-01-02 01:57:48 +0100

    changed path

--- stocks/quotes/tests.py
+++ stocks/quotes/tests.py
@@ -1,9 +1,1 @@
-import requests
-import json
 
-api_request = requests.get(
-            "https://sandbox.iexapis.com/stable/stock/"
-            + "goog"
-            + "/quote?token=Tpk_1ffd0fa5cbb241889288941e2f4e0e0f"
-        ).json()
-print(api_request)
